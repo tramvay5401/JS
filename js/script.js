ac6a52f634b8814680ed5bf0dbbea0ff8f585aba
tramvay5401 <tramvay5401@gmail.com>
2020-01-10 12:26:48 +0200

    this is start

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// var leftBorderWidtsh = 1;
+// const pi = 3.14;
+// {
+//     let second = 2;
+// }
+// var number = 5;
+// var string = 'Hello';
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+
+// var und;
+// console.log(und);
+// obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj["name"]);
+// var arr = ['plum', 'orange', 'Apple'];
+// console.log(arr[2]);
+
+// alert('heelo');
+// confirm('Are you here?')
+// var ansver = +prompt('vam est 18?', '18')
+// console.log(typeof (arr));
